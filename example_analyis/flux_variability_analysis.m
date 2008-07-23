9645e51943cd0c6c3ff2f10c61b749ed12a591a4
Michael Barton <mail@michaelbarton.me.uk>
2008-07-23 16:43:23 +0000

    Example analysis to test reaction sensitivity of all available yeast genes under 5 different nutrient limiting conditions.

---
+++ example_analyis/flux_variability_analysis.m
@@ -0,0 +1,48 @@
+addpath('helpers');setup;
+originalModel = readCbModel('models/Sc_iND750_GlcMM.xml');
+
+genes = [];
+reaction_names =[];
+relative_costs = [];
+absolute_costs = [];
+r_squares = [];
+environments =[];
+
+exchanges = {'EX_glc(e)','EX_nh4(e)','EX_o2(e)','EX_pi(e)','EX_so4(e)'};
+
+biomassFix = 0.1;
+biomassReaction = 'biomass_SC4_bal';
+
+for e = 1:length(exchanges)
+  exchange = exchanges(e);
+
+  model = fixGrowthOptimiseUptake(originalModel,biomassReaction,exchange,biomassFix);
+
+  for g = 1:length(model.genes)
+
+    % All the reactions catalysed by the gene
+    reactions = model.rxns(find(model.rxnGeneMat(:,g)));
+
+    for r = 1:length(reactions)
+
+      % Find the optimal flux for this reaction
+      reaction = reactions(r);
+
+      [relative,absolute,r] = calculateReactionSensitivity(model,reaction);
+
+      genes = [genes model.genes(g)];
+      reaction_names = [reaction_names reaction];
+      environments = [environments exchange];
+      
+      relative_costs = [relative_costs relative];
+      absolute_costs = [absolute_costs absolute];
+      r_squares = [r_squares r];
+
+    end
+  end
+end
+
+% Print out reaction data
+file = 'reaction_fluxes.txt';
+header = {'gene','reaction','environment','relative','absolute','r_square'};
+printLabeledData([genes',reaction_names',environments'],[relative_costs',absolute_costs',r_squares'],false,false,file,header);
