c4b538b24e63f68a3b965e02cf88cf0bcedcbc4b
Michael Barton <mail@michaelbarton.me.uk>
2008-07-23 12:05:57 +0000

    Bug fixes in calculating reaction sensitivity

--- helpers/calculateReactionSensitivity.m
+++ helpers/calculateReactionSensitivity.m
@@ -1,20 +1,24 @@
-function sensitivity = calculateReactionSensitivity(model,controlRxn)
+function [relative,absolute,r] = calculateReactionSensitivity(model,controlRxn)
   reaction_number = findRxnIDs(model,controlRxn);
   solution = optimizeCbModel(model);
   optimal_reaction_flux = solution.x(reaction_number);
 
   if optimal_reaction_flux == 0
+    relative = 0;
+    absolute = 0;
+    r = 1;
+  else
     controlFlux = linspace(optimal_reaction_flux,optimal_reaction_flux * 0.999,5);
-    objFlux = [];
+    objFlux = zeros(1,5);
 
     for i=1:length(controlFlux)
       modelControlled = changeRxnBounds(model,controlRxn,controlFlux(i),'b');
       solControlled = optimizeCbModel(modelControlled);
       objFlux(i) = solControlled.f;
     end
-			    
-    regression = polyfit(controlFlux,objFlux,1);
-    sensitivity = regression(1);
-  else
-    sensitivity = 0;
+			
+    [coefficients,confidence,residuals,intervals,stats] = regress(objFlux',[ones(5,1),controlFlux']);
+    relative = coefficients(1);
+    absolute = relative * (100 / optimal_reaction_flux);
+    r = stats(1);
   end
