3cdd555fce1116fd7975ab1fec2bea6fdb3c30ce
Michael Barton <mail@michaelbarton.me.uk>
2008-07-24 10:56:01 +0000

    Updated calculate reaction sensitivity helper. Now does calculates a single perturbation point, and calculates the differential.

--- helpers/calculateReactionSensitivity.m
+++ helpers/calculateReactionSensitivity.m
@@ -1,28 +1,23 @@
-function [relative,absolute,r] = calculateReactionSensitivity(model,controlRxn,numberOfPoints)
+function [relative,absolute] = calculateReactionSensitivity(model,controlRxn)
   reaction_number = findRxnIDs(model,controlRxn);
   solution = optimizeCbModel(model);
-  optimal_reaction_flux = solution.x(reaction_number);
 
-  if nargin < 3
-    numberOfPoints = 7;
-  end
+  reactionFlux  = solution.x(reaction_number);
+  objectiveFlux = solution.f
 
-  if optimal_reaction_flux == 0
+  if reactionFlux == 0
     relative = 0;
     absolute = 0;
-    r = 1;
   else
-    controlFlux = linspace(optimal_reaction_flux,optimal_reaction_flux * 0.9999,numberOfPoints);
-    objFlux = zeros(1,numberOfPoints);
 
-    for i=1:length(controlFlux)
-      modelControlled = changeRxnBounds(model,controlRxn,controlFlux(i),'b');
-      solControlled = optimizeCbModel(modelControlled);
-      objFlux(i) = solControlled.f;
-    end
-			
-    [coefficients,confidence,residuals,intervals,stats] = regress(objFlux',[ones(numberOfPoints,1),controlFlux']);
-    relative = coefficients(1);
-    absolute = relative * (100 / optimal_reaction_flux);
-    r = stats(1);
+    peturbedReactionFlux = reactionFlux * 0.999;
+    peturbedModel = changeRxnBounds(model,controlRxn,peturbedReactionFlux,'b');
+    peturbedSolution = optimizeCbModel(peturbedModel);
+    peturbedObjectiveFlux = peturbedSolution.f;
+
+    dObj = objectiveFlux = peturbedObjectiveFlux - objectiveFlux;
+    dReaction = reactionFlux - peturbedReactionFlux
+
+    relative = dObj / dReaction
+    absolute = relative * (100 / reactionFlux);
   end
