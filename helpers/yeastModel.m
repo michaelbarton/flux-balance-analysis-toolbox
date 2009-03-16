3e5da2d2fe8e4f19019eb7e1ba637ca3681466b4
Michael Barton <mail@michaelbarton.me.uk>
2009-03-16 15:06:55 +0000

    Yeast model loading helper

---
+++ helpers/yeastModel.m
@@ -0,0 +1,8 @@
+function model = yeastModel()
+
+  model = readCbModel('models/Sc_iND750_GlcMM.xml');
+
+  % Block malate, oxoglutarate, and alpha-ketoglutarate uptakes
+  model = changeRxnBounds(model, 'AKGMAL', 0, 'u');
+  model = changeRxnBounds(model, 'AKGt2r', 0, 'u');
+  model = changeRxnBounds(model, 'MALt2r', 0, 'b');
