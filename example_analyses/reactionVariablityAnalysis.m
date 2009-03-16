e01a40351b8d36669533f167f8194de71e096f50
Michael Barton <mail@michaelbarton.me.uk>
2009-03-16 16:41:52 +0000

    Focus on simple respiratory growth

--- example_analyses/reactionVariablityAnalysis.m
+++ example_analyses/reactionVariablityAnalysis.m
@@ -16,68 +16,53 @@ row = 0;
 for e = 1:length(exchanges)
   exchange = exchanges(e);
 
-  respiratoryModel  = fixGrowthOptimiseUptake(model,biomassReaction,exchange,biomassFix);
-  respiratoryModel.energy = 'respiratory';
-  fermentativeModel = createFermentativeModel(respiratoryModel,exchange);
-  fermentativeModel.energy = 'fermentative';
-  models    = [respiratoryModel,fermentativeModel];
-
-  respiratorySolution = optimizeCbModel(respiratoryModel);
-  fermentativeSolution = linearMOMA(respiratoryModel,fermentativeModel);
-  solutions = struct;
-  solutions(1).x = respiratorySolution.x;
-  solutions(2).x = fermentativeSolution.x;
+  uptakeModel  = fixGrowthOptimiseUptake(model,biomassReaction,exchange,biomassFix);
+  solution = optimizeCbModel(uptakeModel);
 
   for r = 1:length(reactions)
 
-     for m = 1:length(models)
-	this_model = models(m);
-	this_solution = solutions(m);
+    reaction      = reactions(r);
+    reaction_name = model.rxns(reaction);
 
-        reaction      = reactions(r);
-        reaction_name = model.rxns(reaction);
+    row = row + 1;
+    results(row).gene              = model.genes(find(model.rxnGeneMat(reaction,:)));
+    results(row).reaction          = reaction;
+    results(row).environment       = exchange;
 
-        row = row + 1;
-        results(row).gene              = model.genes(find(model.rxnGeneMat(reaction,:)));
-        results(row).reaction          = reaction;
-        results(row).environment       = exchange;
-        results(row).energy            = {this_model.energy};
-
-	% Skip this reaction if not used
-	if this_solution.x(reaction) == 0
-	  results(row).type = {'zero flux'};
-	  continue;
-	end
-
-	if this_solution.x(reaction)^2 == 1000^2
-	  results(row).type = {'at maximum'};
-	  continue;
-	end
-
-	% Determine variability of this reaction
-	temp = changeObjective(this_model,reaction_name);
-	if this_solution.x(reaction) < 0
-	  % Find maximum of reaction
-	  minimumSolution = optimizeCbModel(temp,'max');
-	else
-	  % Find minimum of reaction
-	  minimumSolution = optimizeCbModel(temp,'min');
-	end
-
-	% Skip reaction if it cannot vary in solution space
-	% accounting for binary imprecision
-	if round(minimumSolution.x(reaction)*10^7) == round(this_solution.x(reaction)*10^7)
-	  results(row).type = {'constrained'};
-	  continue;
-	end
-
-        results(row).type = {'variable'};
-
-      end
+    %Skip this reaction if not used
+    if solution.x(reaction) == 0
+      results(row).type = {'zero flux'};
+      continue;
     end
+
+    if solution.x(reaction)^2 == 1000^2
+      results(row).type = {'at maximum'};
+      continue;
+    end
+
+    % Determine variability of this reaction
+    temp = changeObjective(uptakeModel,reaction_name);
+    if solution.x(reaction) < 0
+      % Find maximum of reaction
+      minimumSolution = optimizeCbModel(temp,'max');
+    else
+      % Find minimum of reaction
+      minimumSolution = optimizeCbModel(temp,'min');
+    end
+
+    % Skip reaction if it cannot vary in solution space
+    % accounting for binary imprecision
+    if round(minimumSolution.x(reaction)*10^7) == round(solution.x(reaction)*10^7)
+      results(row).type = {'constrained'};
+      continue;
+    end
+
+    results(row).type = {'variable'};
+
+  end
 end
 
 % Print out reaction data
 file = 'gene_reaction_type.txt';
-header = {'gene','environment','energy','type','reaction'};
-printLabeledData([[results.gene]',[results.environment]',[results.energy]',[results.type]'],[[results.reaction]'],false,-1,file,header);
+header = {'gene','environment','type','reaction'};
+printLabeledData([[results.gene]',[results.environment]',[results.type]'],[[results.reaction]'],false,-1,file,header);
