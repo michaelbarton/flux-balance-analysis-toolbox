5aed2142e0038a71b276377799d9c8194e8f15b9
Michael Barton <mail@michaelbarton.me.uk>
2009-11-04 15:35:13 +0000

    Revised comments in helpers

--- helpers/fixGrowthOptimiseUptake.m
+++ helpers/fixGrowthOptimiseUptake.m
@@ -1,10 +1,12 @@
 function updated_model = fixGrowthOptimiseUptake(model,biomassShort,rxnShort,biomassFix)
 
+  % Use this as the default biomass fix if not defined
+  % in the method arguments
   if (nargin < 4)
     biomassFix = 0.05;
   end
 
-  % Set all exchange reactions to -1000
+  % Set all exchange reactions to effectively unlimited
   [selExc,selUptake] = findExcRxns(model,false,false);
   exchanges = find(selUptake);
   temp = changeRxnBounds(model, model.rxns(exchanges), -100000, 'l');
